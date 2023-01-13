f3b9c4ee1a9be4e717897c0e83e6681e4ac208c4
Moachuba-Tomato <nutjobflix@gmail.com>
2023-01-13 14:26:42 +0530

    Aces arena first Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+const routes: Routes = [
+
+  {path: '', pathMatch: 'full', redirectTo: 'home',  component: AppComponent}, 
+  {path: 'navbar', component: NavbarComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
